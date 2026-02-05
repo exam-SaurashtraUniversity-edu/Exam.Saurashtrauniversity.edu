a97fe01b228c65ce7dd64218ab7f4efd57de4d6b
exam-SaurashtraUniversity-edu <kantilalredmi8@gmail.com>
2026-02-06 04:15:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,34 @@
-function login() {
-  let email = document.getElementById("email").value;
-  let password = document.getElementById("password").value;
+function login() {    
+  let email = document.getElementById("email").value;    
+  let password = document.getElementById("password").value;    
 
-  if (email === "A" && password === "s") {
-    window.location.href = "image2.html"; // go to 2nd image (profile)
-  } else {
-    alert("Wrong email or password");
-  }
-}
+  if (email === "A" && password === "s") {    
+    window.location.href = "image2.html";    
+  } else {    
+    alert("Wrong email or password");    
+  }    
+}    
 
-function goMenu() {
-  window.location.href = "image3.html"; // go to 3rd image (menu)
-}
+function goMenu() {    
+  window.location.href = "image3.html";    
+}    
 
-function openNextImage(current) {
-  if (current === 4) window.location.href = "image5.html"; // 4th → 5th
-}
+function openNextImage(current) {    
+  if (current === 4) window.location.href = "image5.html";    
+  if (current === 6) window.location.href = "image6.html";    
+  if (current === 7) window.location.href = "image7.html";    
+  if (current === 8) window.location.href = "image8.html";    
+  if (current === 9) window.location.href = "image9.html";    
+}    
 
-function checkDownload(e) {
-  let x = e.clientX / window.innerWidth;
-  let y = e.clientY / window.innerHeight;
+function checkDownload(e) {    
+  let x = e.clientX / window.innerWidth;    
+  let y = e.clientY / window.innerHeight;    
 
-  if (x >= 0.50 && x <= 0.74 && y >= 0.75 && y <= 0.80) {
-    const link = document.createElement("a");
-    link.href = "203_3_2025 - Marksheet.pdf"; // updated file name
-    link.download = "203_3_2025 - Marksheet.pdf"; // updated download name
-    link.click();
-  }
+  if (x >= 0.50 && x <= 0.74 && y >= 0.75 && y <= 0.80) {    
+    const link = document.createElement("a");    
+    link.href = "203_3_2025 - Marksheet.pdf";    
+    link.download = "203_3_2025 - Marksheet.pdf";    
+    link.click();    
+  }    
 }
